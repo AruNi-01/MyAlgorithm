f9e4fc40039883afcdd807d45e6a3bb9cfc4caf2
AruNi <1298911600@qq.com>
2022-04-21 23:45:08 +0800

    BT

--- LeetCode/BinaryTree/postorder_traversal.cpp
+++ LeetCode/BinaryTree/postorder_traversal.cpp
@@ -71,3 +71,31 @@ public:
     }
 };
 
+/*
+迭代法的前中后序遍历统一写法：
+*/
+class Solution{
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        if (root != NULL) st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();      // 每次都让node指向栈顶元素
+            if (node != NULL) {
+                st.pop();     // 将该结点弹出，避免重复操作；下面再将右中左结点添加到栈中
+                st.push(node);      // 添加中节点
+                st.push(NULL);      // 中节点访问过，但是还没有处理，加入空节点作为标记
+
+                if (node->right) st.push(node->right);      // 添加右节点
+                if (node->left) st.push(node->left);    // 添加左节点
+            } else {        // 只有遇到空节点时，才将下一个节点放入结果集
+                st.pop();           // 将空节点弹出
+                node = st.top();        //重新取出栈中元素
+                st.pop();
+                res.push_back(node->val);       // 加入到结果集
+            }
+        }
+        return res;
+    }
+};
--- LeetCode/BinaryTree/preorder_traversal.cpp
+++ LeetCode/BinaryTree/preorder_traversal.cpp
@@ -68,3 +68,37 @@ public:
         return res;
     }
 };
+
+/*
+迭代法的前中后序遍历统一写法：
+将访问的节点放入栈中，把要处理的节点也放入栈中但是要做标记。
+如何标记呢，就是要处理的节点放入栈之后，紧接着放入一个空指针作为标记。 
+这种方法也可以叫做标记法；过程有些抽象，一定要画图理解！
+图解：https://tva1.sinaimg.cn/large/008eGmZEly1gnbmq3btubg30em09ue82.gif
+*/
+class Solution{
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        if (root != NULL) st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();      // 每次都让node指向栈顶元素
+            if (node != NULL) {
+                st.pop();     // 将该结点弹出，避免重复操作；下面再将右中左结点添加到栈中
+                if (node->right) st.push(node->right);      // 添加右节点
+                if (node->left) st.push(node->left);        // 添加左节点
+                st.push(node);      // 添加中节点
+                st.push(NULL);      // 中节点访问过，但是还没有处理，加入空节点作为标记
+
+                
+            } else {        // 只有遇到空节点时，才将下一个节点放入结果集
+                st.pop();           // 将空节点弹出
+                node = st.top();        //重新取出栈中元素
+                st.pop();
+                res.push_back(node->val);       // 加入到结果集
+            }
+        }
+        return res;
+    }
+};
