be8ca19ea6ff22cd1022c9aa860b6fa8d47a7819
AruNi <1298911600@qq.com>
2022-05-02 23:55:54 +0800

    二叉树

---
+++ LeetCode/BinaryTree/maximum_binary_tree.cpp
@@ -0,0 +1,75 @@
+/*
+ * @Descripttion: 654. 最大二叉树
+ * @Author: AruNi
+ * @Date: 2022-05-02 23:43:22
+ */
+
+/*
+题目：
+给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+    创建一个根节点，其值为 nums 中的最大值。
+    递归地在最大值 左边 的 子数组前缀上 构建左子树。
+    递归地在最大值 右边 的 子数组后缀上 构建右子树。
+返回 nums 构建的 最大二叉树 。
+
+示例 1：
+输入：nums = [3,2,1,6,0,5]
+输出：[6,3,5,null,2,0,null,null,1]
+        6
+       / \
+      3   5
+      \  /
+      2 0
+       \
+        1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// 递归法：前序遍历，先构造最大的根节点
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        TreeNode* node = new TreeNode(0);
+        // 退出条件：数组size = 1(叶子节点)；因为nums.length>=1
+        if (nums.size() == 1) {
+            node->val = nums[0];
+            return node;
+        }
+
+        // 找到数组中最大的值和对应的下标
+        int maxValue = 0;
+        int maxValueIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > maxValue) {
+                maxValue = nums[i];
+                maxValueIndex = i;
+            }
+        }
+        // 将最大值赋给当前节点
+        node->val = maxValue;
+
+        // 最大值所在的下标左区间，递归构造左子树
+        if (maxValueIndex > 0) {
+            vector<int> newVec(nums.begin(), nums.begin() + maxValueIndex);
+            node->left = constructMaximumBinaryTree(newVec);
+        }
+        // 最大值所在的下标右区间，递归构造右子树
+        if (maxValueIndex < (nums.size() - 1)) {
+            vector<int> newVec(nums.begin() + maxValueIndex + 1, nums.end());
+            node->right = constructMaximumBinaryTree(newVec);
+        }
+
+        return node;
+    }
+};
